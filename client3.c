10219fe05d6b7f703bc3e47398ad262668c3d246
Vladimir Blinov <vovka_squid@mail.ru>
2015-06-04 18:20:29 +0300

    push
    Signed-off-by: Vladimir Blinov <vovka_squid@mail.ru>

--- client3.c
+++ client3.c
@@ -5,32 +5,81 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/ioctl.h>
 
-int main()
+#define BUFF_SIZE 512
+#define name_size 10
+
+int sockfd;
+
+void *listener()
 {
-	int sockfd;
+	int nread;
+	int fd;
+	while (1) {
+
+		char buf1[BUFF_SIZE] = {0};
+		//read(sockfd, &fd, sizeof(fd));
+		nread = read(sockfd, buf1, BUFF_SIZE + name_size - 1);
+		if (nread != 0) {
+			//printf("%d", fd);
+			printf("From server -> %s", buf1);
+		}
+		else
+			exit(1);
+	}
+}
+
+int main(void)
+{
+
+	pthread_t a_thread;
+	char name[name_size];
 	int len;
 	struct sockaddr_in address;
 	int result;
-	char ch[10] = "abcd";
-	//Создаём соект клиента
+	char buf[BUFF_SIZE],buf_sup[BUFF_SIZE + name_size];
+	pthread_attr_t thread_attr;
+
+	pthread_attr_init(&thread_attr);
+
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	//Присваиваем имя сокету по согласованию с сервом
+
+	printf("Hello! Input your name\n");
+	fgets(name, name_size, stdin);
+	//name_len = srtlen(name);
+	//printf("name = %s \n", name);
+	////strcpy(buf_sup, name);
+	//buf_sup[strlen(buf_sup) - 1] = ':';
+	//printf("buf_sup = %s", buf_sup);
+	////int n = strlen(buf_sup);
+	////buf_sup[n-1] = ':';
+	//printf("buf_sup = %s \n", buf_sup);
+	//printf("strlen = %d \n", n);
+	
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = inet_addr("127.0.0.1");
 	address.sin_port = htons(9734);
 	len = sizeof(address);
-	//connect our socket with server socket
 	result = connect(sockfd, (struct sockaddr *)&address, len);
-
 	if (result == -1) {
-		perror("oops: client1");
+		perror("error : client");
 		exit(1);
 	}
-	//Write and read using sockfd;
-	write(sockfd, &ch, 1);
-	read(sockfd, &ch, 1);
-	printf("char from server = %s\n", ch);
-	close(sockfd);
-	exit(0);
+
+	pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED);
+	pthread_create(&a_thread, &thread_attr, &listener, (void *)0);
+
+	while (1) {
+		char buf_sup[BUFF_SIZE + name_size] = {0};
+		char buf1[BUFF_SIZE + name_size] = {0};	
+		strcpy(buf_sup, name);
+		int n = strlen(buf_sup);
+		buf_sup[n-1] = ':';
+		fgets(buf, BUFF_SIZE, stdin);
+		strcat(buf_sup, buf); //printf("bf = %s", buf_sup);
+		write(sockfd, buf_sup, strlen(buf_sup));
+	}
 }
--- server5.c
+++ server5.c
@@ -1,19 +1,29 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
-#include <netinet/in.h>
-#include <sys/time.h>
-#include <sys/ioctl.h>
+#include <sys/un.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
+#define buf_size 512
+#define name_size 10
 int main()
 {
 	int server_sockfd, client_sockfd;
 	int server_len, client_len;
+	int i;
+	int currentFd = -1;
 	struct sockaddr_in server_address;
 	struct sockaddr_in client_address;
 	int result;
+	int sockStorage[20] ={0};
+	int storageSave = 0;
+	char buf[buf_size + name_size];
 	fd_set readfds, testfds;
 	//Создаём сокет для сервера и присваиваем ему имя
 	server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -34,7 +44,7 @@ int main()
 	FD_SET(server_sockfd, &readfds);
 	//Ждём запросов. Если select вернёт < 1, то всё плохо
 	while(1) {
-		char ch[10];
+		
 		int fd;
 		int nread;
 	
@@ -50,6 +60,7 @@ int main()
 		}
 		//Выясняем какой из дескриптеров активен
 		for (fd = 0; fd < FD_SETSIZE; fd++) {
+			//write(fd, "test", 1);
 			if (FD_ISSET(fd, &testfds)) {
 		//если активность на server_sockfds, то это может
 		//быть запрос на новое соединение, добавляем новый
@@ -59,26 +70,42 @@ int main()
 					client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &client_len);
 					FD_SET(client_sockfd, &readfds);
 					printf("adding client of fd %d\n", client_sockfd);
+					sockStorage[storageSave] = client_sockfd;
+					printf("fd in mas = %d\n",sockStorage[storageSave]);
+					printf("sS = %d\n", storageSave);
+					storageSave++;
 				}
 				//Иначе активность проявляет клиент
 				//Если close, то его можно удалить
 				//из множества дескриптеров
 				//иначе, обслуживаем клиента
 				else {
-					ioctl(fd, FIONREAD, &nread);
-	
+					//ioctl(fd, FIONREAD, &nread);
+					char buf1[buf_size + name_size] = {0};
+					nread = read(fd, buf1, buf_size + name_size - 1);
 					if (nread == 0) {
+						for(i = 0; i < 20; ++i) 
+							if(sockStorage[i] == fd)
+								sockStorage[i] = -1;
 						close(fd);
 						FD_CLR(fd, &readfds);
 						printf("removing client on fd %d\n", fd);
-}
-					else {
-						read(fd, &ch, 1);
-						sleep(2);
-						printf("serving client on fd %d\n", fd);
-						//ch++;
-						write(fd, &ch, 1);
 					}
+
+					else {// Пишем во все "живые сокеты"
+						for(i = 0; i < storageSave; ++i)
+							if((sockStorage[i] > 0) && (fd != sockStorage[i])) {
+								printf("fd = %d\n", fd);								
+								printf("i = %d\n", i);
+								printf("sSF = %d\n", storageSave);
+								printf("nread = %d \n", nread);
+								printf("string = %s \n", buf1);
+								printf("sock to write = %d \n", sockStorage[i]);
+								
+								write(sockStorage[i], &buf1, nread);
+							}
+					}
+					
 				}
 			}
 		}
